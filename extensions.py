e8ef492a38d63c02069c6ad49fe1b46035c658cd
mch <maksim.cherepanov.88.88@bk.ru>
2025-11-13 14:49:50 +0300

    изменил фильтр товаров на главной странице и на странице редактирование товаров, добавил логирование при попытке входа с отозванными токенами

--- extensions.py
+++ extensions.py
@@ -1,4 +1,5 @@
 # Модуль для избежания циклический импортов.
+import logging
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
@@ -10,12 +11,21 @@ migrate = Migrate()
 jwt = JWTManager()
 mail = Mail()
 
+sys_logger = logging.getLogger('app.system')
 
-# Колбэк для проверки отзыва токена
 
+# Декоратор из flask-jwt-extended, который регистрирует функцию, вызываемую каждый раз, когда поступает запрос с JWT-токеном.
 @jwt.token_in_blocklist_loader
 def check_if_token_revoked(_, jwt_payload):
-    jti = jwt_payload["jti"]
+    jti = jwt_payload.get("jti")
+    user_id = jwt_payload.get("sub", "unknown")
+
     from models import UserToken
     token = UserToken.query.filter_by(jti=jti).first()
-    return token is not None and token.revoked
+
+    is_revoked = token is not None and token.revoked
+
+    if is_revoked:
+        sys_logger.warning(f"Обнаружена попытка использования ОТЗВАННОГО токена! user_id={user_id}, jti={jti}")
+
+    return is_revoked
